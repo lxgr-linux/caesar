7bf3f391c3f323cd42ecf9e2af697f37ba798a81
lxgr-linux <lxgr@protonmail.com>
2021-09-15 12:43:46 +0200

    fns

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,39 @@ fn caesar(x:i32, s:i32, t:i32) -> i32{
     ((x*s)+t) % 26
 }
 
+fn string_to_nums(s:&str) -> Vec<i32>{
+    let mut list = vec!();
+    let abc:Vec<char> = String::from("abcdefghijklmnopqrstuvwxyz")
+                                .chars().collect();
+    
+    for cha in s.chars(){
+        list.push(abc.iter()
+                  .position(|&r| cha == r)
+                  .unwrap() as i32);
+    }
+    list
+}
+
+fn nums_to_string(nums:Vec<i32>) -> String{
+    let mut s = String::new();
+    let abc:Vec<char> = String::from("abcdefghijklmnopqrstuvwxyz")
+                                .chars().collect();
+    
+    for num in nums.iter(){
+        s.push(abc[*num as usize]);
+    }
+    s
+}
+
 fn main(){
-    let ls = [10, 0, 13, 13];
+    let kt = "hallowelt";
     let s = 3;
     let t = 7;
+    let mut gt_nums = vec!();
 
-    for x in ls.iter(){
-        println!("{}", caesar(*x, s, t))
-    }   
+    let nums = string_to_nums(&kt);
+    for num in nums.iter(){
+        gt_nums.push(caesar(*num, s, t));
+    }
+    println!("{}", nums_to_string(gt_nums));
 }
